9af6f1a0d4f76674a19e0ebf389ccb81a920be47
austinb847 <austinb847@icloud.com>
2020-03-30 16:53:38 -0700

    add map method instead of foreach to get sorted list

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,20 @@ $(document).ready(function() {
   $("#groceryForm").submit(function(event) {
     $("#groceryForm").hide();
     var itemIDs = ["item1", "item2", "item3", "item4"];
-    var itemVals = [];
-    itemIDs.forEach(function(item) {
+
+    var itemValues = itemIDs.map(function(item) {
+      return $("#" + item).val()
+    }); 
+
+    console.log(itemValues);
+    
+    var sortedItems = itemValues.sort();
+
+    /* itemIDs.forEach(function(item) {
       var userInput = $("#" + item).val();
       itemVals.push(userInput);
     });
-    var sortedItems = itemVals.sort();
+    var sortedItems = itemVals.sort(); */
 
     var uppercaseItems = sortedItems.map(function(item) {
       return item.toUpperCase();
