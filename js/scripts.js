70f5e7d1fe0d8c91d590d1fea5cbb87e64f949d6
austinb847 <austinb847@icloud.com>
2020-03-30 16:07:36 -0700

    add for each loop to get user input values and store them in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
   $("#groceryForm").submit(function(event) {
+    $("#groceryForm").hide();
+    var itemIDs = ["item1", "item2", "item3", "item4"];
+    var itemVals = [];
+    itemIDs.forEach(function(item) {
+      var userInput = $("#" + item).val();
+      itemVals.push(userInput);
+    });
     
-
-
     
     
 
